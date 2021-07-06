092f3d9e41c810d48097f78ca8a5057a28f722cd
saiashish9 <saiashish3760@gmail.com>
2021-07-07 02:36:55 +0530

    completed

---
+++ java/src/com/programming/Main.java
@@ -0,0 +1,36 @@
+package com.programming;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Process process = Runtime.getRuntime().exec("ls -l");
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader (process.getInputStream()));
+            String line;
+            while((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                System.out.println("Something abnormal has haapened :( ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// https://softorks.com/en/java/how-to-execute-shell-command-java-runtimeExec.php
+
+// javac Main.java
+// java Main.java
